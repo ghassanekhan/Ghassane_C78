249b63c29ad420c5dac8c3d3bf36d07522a90012
ghassanekhan <152647582+ghassanekhan@users.noreply.github.com>
2024-05-24 19:07:17 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var first_name = "Ghassane";
+var last_name = "Khan"
+
+var a = 10;
+var b = 5;
+
+var add = a + b ;
+console.log(add);
+
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+
+
